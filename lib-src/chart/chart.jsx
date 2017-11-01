a972da776fa9ec8f9927b74fdd33319763075f19
kongwentao <kongwentao@baidu.com>
2017-11-01 10:36:53 +0800

    add legend to chart

--- lib-src/chart/chart.jsx
+++ lib-src/chart/chart.jsx
@@ -29,7 +29,10 @@ class Chart extends React.Component {
                     formatter: '{value}'
                 }
             },
-            series: []
+            series: [],
+            legend: {
+                data: []
+            },
         }
 
         if (this.props.theme === 'radio') {
@@ -108,6 +111,7 @@ class Chart extends React.Component {
         let data = this.props.dataSource[this.state.activeIndex];
         this.option.xAxis.data = data.xAxis;
         this.option.series = data.series;
+        this.option.legend = data.legend;
         if (this.props.theme === 'radio') {
             this.option.series[0].itemStyle = {
                 normal: {
@@ -173,8 +177,8 @@ class Chart extends React.Component {
         if (this.props.isShowFold === true) {
             openButton = (<div className="hui-chart-panel-unfold"
                 title={(this.state.isFold ? '展开' : '收起') + "面板"}
-                    onClick={this.handleToggleFold.bind(this)}>
-                    <UnfoldIcon/>
+                onClick={this.handleToggleFold.bind(this)}>
+                <UnfoldIcon />
             </div>);
         }
 
@@ -189,11 +193,11 @@ class Chart extends React.Component {
                 <div className="hui-chart-title">
                     {this.getTitleBtn()}
                     {this.props.hideCloseBtn === true ? null :
-                    <svg
-                        className="hui-chart-close" width="30" height="30" viewBox="0 0 1024 1024"
-                        onClick={this.close.bind(this)}>
-                        <path d="M557.312 513.248l265.28-263.904c12.544-12.48 12.608-32.704 0.128-45.248-12.512-12.576-32.704-12.608-45.248-0.128l-265.344 263.936-263.04-263.84C236.64 191.584 216.384 191.52 203.84 204 191.328 216.48 191.296 236.736 203.776 249.28l262.976 263.776L201.6 776.8c-12.544 12.48-12.608 32.704-0.128 45.248 6.24 6.272 14.464 9.44 22.688 9.44 8.16 0 16.32-3.104 22.56-9.312l265.216-263.808 265.44 266.24c6.24 6.272 14.432 9.408 22.656 9.408 8.192 0 16.352-3.136 22.592-9.344 12.512-12.48 12.544-32.704 0.064-45.248L557.312 513.248z"></path>
-                    </svg>
+                        <svg
+                            className="hui-chart-close" width="30" height="30" viewBox="0 0 1024 1024"
+                            onClick={this.close.bind(this)}>
+                            <path d="M557.312 513.248l265.28-263.904c12.544-12.48 12.608-32.704 0.128-45.248-12.512-12.576-32.704-12.608-45.248-0.128l-265.344 263.936-263.04-263.84C236.64 191.584 216.384 191.52 203.84 204 191.328 216.48 191.296 236.736 203.776 249.28l262.976 263.776L201.6 776.8c-12.544 12.48-12.608 32.704-0.128 45.248 6.24 6.272 14.464 9.44 22.688 9.44 8.16 0 16.32-3.104 22.56-9.312l265.216-263.808 265.44 266.24c6.24 6.272 14.432 9.408 22.656 9.408 8.192 0 16.352-3.136 22.592-9.344 12.512-12.48 12.544-32.704 0.064-45.248L557.312 513.248z"></path>
+                        </svg>
                     }
                 </div>
                 <div
